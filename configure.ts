8bb458ae7a85fcbf9f72889c40de47e50fd0ddaa
Harminder Virk <virk.officials@gmail.com>
2023-12-24 13:02:32 +0530

    refactor: configure hook

--- configure.ts
+++ configure.ts
@@ -8,14 +8,18 @@
  */
 
 import type Configure from '@adonisjs/core/commands/configure'
+import { stubsRoot } from './stubs/main.js'
 
 /**
  * Configures the package
  */
 export async function configure(command: Configure) {
-  await command.publishStub('static/config.stub')
-
   const codemods = await command.createCodemods()
+
+  /**
+   * Publish config to "config/static.ts" file
+   */
+  await codemods.makeUsingStub(stubsRoot, 'static/config.stub', {})
 
   /**
    * Register static middleware
